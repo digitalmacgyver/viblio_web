30cbba67fdf93cae91ee070c7d3fe13caa5a5d55
Andrew Peebles <peebles@video-analytics-inc.com>
2013-08-16 10:22:24 -0700

    test for new video arrival

--- durandal01/App/viewmodels/upload.js
+++ durandal01/App/viewmodels/upload.js
@@ -4,6 +4,9 @@ define( ['lib/dialogs','lib/viblio','lib/config','facebook'], function(dialogs,v
 	message: function() {
 	    dialogs.showMessage( 'This is a body', 'Title' );
 	},
+	testNewVideo: function() {
+	    viblio.api( '/services/test/new_video_test' );
+	},
 	linkFacebookAccount: function() {
 	    var fb_appid   = config.facebook_appid();
 	    var fb_channel = config.facebook_channel();
