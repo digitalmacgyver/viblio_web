239f40e2bbc8b99a9d4f57316dd9bcde0d046c28
Andrew Peebles <peebles@video-analytics-inc.com>
2013-09-04 10:51:51 -0700

    added geo map strip to home

--- durandal02/app/viewmodels/home.js
+++ durandal02/app/viewmodels/home.js
@@ -2,7 +2,7 @@
   The channels page, which I think is the default page for an authenticated
   user.
 */
-define(['durandal/app','durandal/system','viewmodels/hscroll','viewmodels/pscroll','lib/customDialogs'],function(app,system,HScroll,PScroll,customDialogs) {
+define(['durandal/app','durandal/system','viewmodels/hscroll','viewmodels/pscroll','viewmodels/mapstrip','lib/customDialogs'],function(app,system,HScroll,PScroll,MapStrip,customDialogs) {
     // A list of horizontal media display lists
     var strips = ko.observableArray([]);
     
@@ -18,16 +18,26 @@ define(['durandal/app','durandal/system','viewmodels/hscroll','viewmodels/pscrol
         } ).promise();
     }
 
+    function mm() {
+        return system.defer( function( dfd ) {
+            dfd.resolve( new MapStrip() );
+        } ).promise();
+    }
+
     return {
         displayName: 'Channels',
         strips: strips,
         activate: function () {
 	    var self = this;
 	    self.strips.removeAll();
-	    return $.when( hh('Box Office Hits', 'Your most popular videos'), pp('Top Actors', 'Who\'s in your videos') ).then( function( h1, h2 ) {
-		self.strips.push( h1 );
-		self.strips.push( h2 );
-	    });
+	    return $.when( hh('Box Office Hits', 'Your most popular videos'), 
+			   pp('Top Actors', 'Who\'s in your videos'),
+			   mm()
+			 ).then( function( h1, h2, h3 ) {
+			     self.strips.push( h1 );
+			     self.strips.push( h2 );
+			     self.strips.push( h3 );
+			 });
         },
 	attached: function() {
 	    return system.defer( function( dfd ) {
---
+++ durandal02/app/viewmodels/mapstrip.js
@@ -0,0 +1,93 @@
+define(['durandal/app', 'plugins/router', 'lib/viblio', 'modestmap'], function(app,router,viblio,MM) {
+    var Map = function() {
+	this.points = ko.observableArray([]);
+    };
+
+    Map.prototype.activate = function() {
+	var self = this;
+	// Obtain the list of points to display.  Need to make
+	// a string out of lat, lng to work with knockout, and
+	// need to add asset uuid so we can play the video when
+	// the marker is clicked on.
+	//
+	this.points.push({
+	    lat: 37.7749295, 
+	    lng: -122.4194155,
+	    location: "37.7749295, -122.4194155",
+	    uuid: 'sss1'
+	});
+	this.points.push({
+	    lat: 37.8043722, 
+	    lng: -122.2708026,
+	    location: "37.8043722, -122.2708026",
+	    uuid: 'sss2'
+	});
+	this.points.push({
+	    lat: 37.8715926, 
+	    lng: -122.272747,
+	    location: "37.8715926, -122.272747",
+	    uuid: 'sss3'
+	});
+	return viblio.api( '/services/mediafile/list' ).then( function( json ) {
+	    var cc = 0;
+	    json.media.forEach( function( m ) {
+		if ( m.lat && m.lng ) {
+		    /**
+		    self.points.push({
+			lat: m.lat,
+			lng: m.lng,
+			location: m.lat.toString() + ',' + m.lng.toString(),
+			uuid: m.uuid,
+			thumbnail: m.views.thumbnail.url
+		    });
+		    **/
+		    // FAKE REMOVE ME!!!
+		    self.points()[cc].uuid = m.uuid;
+		    self.points()[cc].thumbnail = '<img class="popover-img" src="' + m.views.thumbnail.url + '" />';
+		    cc += 1;
+		    if ( cc > 2 ) cc = 0;
+		}
+	    });
+	});
+    };
+
+    Map.prototype.compositionComplete = function( view, parent ) {
+	var self = this;
+
+	// Create the map, enable mouse wheel and touch interaction
+	self.map = $(view).htmapl({
+	    touch: true,
+            mousewheel: true
+	});
+	// Create an array of Location objects to center the map
+	// around those points.
+	var ext = [];
+	self.points().forEach( function( p ) {
+	    ext.push( new MM.Location( p.lat, p.lng ) );
+	});
+	if ( ext.length )
+	    self.map.extent( ext );
+	else
+	    console.log( 'NO POINTS' );
+
+	// Enable popovers
+	$(view).find(".marker i").popover({
+	    trigger: 'hover',
+	    html: true
+	});
+
+    };
+
+    Map.prototype.play = function( point, a, b ) {
+	// A point was clicked on.  Could popup a dialog to display
+	// metadata (title, description, captured on date, length, etc)
+	// and a play/cancel function maybe.  Or just go to player screen.
+	console.log( "play", point.uuid );
+	router.navigate( '#/player?mid=' + point.uuid );
+	//console.log( a, b );
+	//$(a.target).popover( 'toggle' );
+    };
+
+    return Map;
+    
+});
