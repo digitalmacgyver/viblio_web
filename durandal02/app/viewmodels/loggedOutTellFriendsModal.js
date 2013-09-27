e63cf90136eb628935debbd75e04d0d298ec4a97
Andrew Peebles <aqpeeb@gmail.com>
2013-09-26 20:22:33 -0700

    facebook like autocomplete tokenizer

--- durandal02/app/viewmodels/loggedOutTellFriendsModal.js
+++ durandal02/app/viewmodels/loggedOutTellFriendsModal.js
@@ -1,16 +1,7 @@
 define( ['plugins/router', 'durandal/app', 'durandal/system', 'lib/config', 'lib/viblio', 'plugins/dialog', 'facebook'], function( router, app, system, config, viblio, dialog ) {
     
-    var friendsEmails = ko.observableArray();
     var friendsEmailsValid = ko.observable(false);
     var tellFriendsMessage = ko.observable();
-    
-    friendsEmails.subscribe( function() {
-        if ( $('#friendsEmails')[0].checkValidity() ) {
-          friendsEmailsValid( true );
-        } else {
-          friendsEmailsValid( false );
-        }
-    });
     
     function showLoggedOutTellFriendsModal() {
         dialog.show('viewmodels/loggedOutTellFriendsModal');
@@ -31,19 +22,36 @@ define( ['plugins/router', 'durandal/app', 'durandal/system', 'lib/config', 'lib
 	if ( ! message ) 
 	    message = $('#tellFriendsMessage').val();
 
-        system.log( friendsEmails(), message );
-
 	// Give the api call time to process
 	viblio.rescheduleLogout( 60 );
-	viblio.api( '/services/user/tell_a_friend', { list: friendsEmails(), message: message } ).then( function() {
-	    self.closeModal();
-	});
+	var list = $(self.view).find( "#friendsEmails" ).val();
+        system.log( list, message );
+
+	//viblio.api( '/services/user/tell_a_friend', { list: list, message: message } ).then( function() {
+	//    self.closeModal();
+	//});
     };
 
     return {
-        friendsEmails: friendsEmails,
         friendsEmailsValid: friendsEmailsValid,
         tellFriendsMessage: tellFriendsMessage,
+
+	compositionComplete: function( view ) {
+	    var self = this;
+	    self.view = view;
+	    $(self.view).find( "#friendsEmails" ).tokenInput( 
+		'/services/faces/contact_emails',
+		{ minChars: 2,
+		  theme: "facebook",
+		  preventDuplicates: true,
+		  onAdd: function() {
+		      self.friendsEmailsValid( true );
+		  },
+		  resultsFormatter: function( item ) {
+		      return '<li>' + item.name + '&nbsp;(' + item.id + ')</li>';
+		  }
+		});
+	},
         
         showLoggedOutTellFriendsModal: showLoggedOutTellFriendsModal,
         closeModal: closeModal,
