818b714fecf0bc89337311968b2f46ad95097eb6
Andrew Peebles <aqpeeb@gmail.com>
2013-10-03 15:04:49 -0700

    beta reserved social posts

--- durandal02/app/viewmodels/betaReserved.js
+++ durandal02/app/viewmodels/betaReserved.js
@@ -1,29 +1,42 @@
 define( ['plugins/router', 'durandal/app', 'durandal/system', 'lib/config', 'lib/viblio', 'plugins/dialog', 'facebook'], function( router, app, system, config, viblio, dialog ) {
     
-    var shareURL = ko.computed(function() {
-        return encodeURIComponent( $(location).attr('href') );
-    });
-    
     var shareTitle = ko.computed(function() {
         return encodeURIComponent( 'Enroll in the private Viblio Beta now!' );
     });
     
-    var shareNetworks = [ { name: 'Facebook', addClass: 'fb', url: 'http://www.facebook.com/share.php?u=' + shareURL(), imgName: 'FBf.png' }, 
-                          { name: 'Twitter', addClass: 'twitter', url: 'https://twitter.com/share?url=' + shareURL() + '&text=' + shareTitle().substring(0,130), imgName: 'twitter.png' }, 
-                          { name: 'Google+', addClass: 'gPlus', url:'https://plusone.google.com/_/+1/confirm?hl=en&url=' + shareURL(), imgName: 'gPlus.png' },
-                          { name: 'tumblr', addClass: 'tumblr', url:'http://www.tumblr.com/share?v=3&u=' + shareURL(), imgName: 'tumblr.png' }
+    var shareNetworks = [ { name: 'Facebook', addClass: 'fb', url: 'http://www.facebook.com/share.php?u=' + facebookLink(), imgName: 'FBf.png' }, 
+                          { name: 'Twitter', addClass: 'twitter', url: 'https://twitter.com/share?url=' + twitterLink() + '&text=' + shareTitle().substring(0,130), imgName: 'twitter.png' }, 
+                          { name: 'Google+', addClass: 'gPlus', url:'https://plusone.google.com/_/+1/confirm?hl=en&url=' + googleLink(), imgName: 'gPlus.png' },
+                          { name: 'tumblr', addClass: 'tumblr', url:'http://www.tumblr.com/share?v=3&u=' + tumblrLink(), imgName: 'tumblr.png' }
                         ];
     
-    fb_appid   = config.facebook_appid();
-    fb_channel = config.facebook_channel();
+    function facebookLink() {
+	var server = window.location.protocol + config.site_server;
+	// Override for testing
+	server = 'http://staging.viblio.com';
+	return encodeURIComponent( server + '/shared/simple' );
+    }
 
-    FB.init({
-	appId: fb_appid,
-	channelUrl: fb_channel,
-	status: true,
-	cookie: true,
-	xfbml: true
-    });
+    function twitterLink() {
+	var server = window.location.protocol + config.site_server;
+	// Override for testing
+	server = 'http://staging.viblio.com';
+	return encodeURIComponent( server + '/shared/simple' );
+    }
+
+    function googleLink() {
+	var server = window.location.protocol + config.site_server;
+	// Override for testing
+	server = 'http://staging.viblio.com';
+	return encodeURIComponent( server + '/shared/simple' );
+    }
+
+    function tumblrLink() {
+	var server = window.location.protocol + config.site_server;
+	// Override for testing
+	server = 'http://staging.viblio.com';
+	return encodeURIComponent( server + '/shared/simple' );
+    }
     
     function closeModal() {
         dialog.close(this);
