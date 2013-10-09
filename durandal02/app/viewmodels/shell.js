9998b83afbf2ad760ece02594b12b563ce24d3e9
Andrew Peebles <aqpeeb@gmail.com>
2013-10-09 13:31:48 -0700

    testing ga

--- durandal02/app/viewmodels/shell.js
+++ durandal02/app/viewmodels/shell.js
@@ -23,8 +23,8 @@ define(['plugins/router','durandal/app','durandal/system','viewmodels/header','v
 	// Log it to analytics
 	ga( 'send', 'pageview', {
 	    title: instruction.config.title,
-	    page: instruction.config.route });
-	console.log( 'route', instruction.config.route );
+	    page: '/' + instruction.config.route });
+	console.log( 'route', '/' + instruction.config.route );
 	if ( instruction.config.authenticated ) {
 	    // If the route is marked authenticated, then do a server
 	    // round trip to make sure we have a session.  If we do
