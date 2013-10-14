1073be9941d41ece6768874334d874c738174fa9
Andrew Peebles <peebles@video-analytics-inc.com>
2013-10-14 10:44:32 -0700

    rework for simpler form

--- durandal02/app/viewmodels/register.js
+++ durandal02/app/viewmodels/register.js
@@ -1,81 +1,42 @@
 define(['plugins/router','lib/viblio','durandal/system'], function( router, viblio, system ) {
 
-    var email = ko.observable();
-    var email_valid = ko.observable(false);
-    var password1 = ko.observable();
-    var password2 = ko.observable();
+    var password = ko.observable();
     var displayname = ko.observable();
-    var edittable_email = ko.observable(true);
-    var accept = ko.observable(false);
-    var terms = ko.observable();
     var validated = ko.computed( function() {
-	return email() && email_valid() && password1() && password2() &&
-	    ( password1() == password2() ) &&
-	    displayname() && accept();
+	return password();
     });
+
+    var email;
     var url;
 
-    var displayname_msg = ko.observable( 'required' );
-    displayname.subscribe( function(v) {
-	if ( v && v != '' )
-	    displayname_msg( 'good' );
-	else
-	    displayname_msg( 'required' );
-    });
+    var labelShowHide = ko.observable( 'reveal' );
 
-    var email_msg = ko.observable( 'required' );
-    email.subscribe( function(v) {
-	if ( v && v != '' ) {
-	    viblio.api( '/services/na/valid_email', { email: v } ).then( function( json ) {
-		if ( json.valid ) {
-		    email_msg( 'good' );
-		    email_valid( true );
-		}
-		else {
-		    email_msg( json.why );
-		    email_valid( false );
-		}
-	    });
-	}
-	else {
-	    email_msg( 'required' );
-	    email_valid( false );
-	}
-    });
-
-    var password1_msg = ko.observable( 'required' );
-    password1.subscribe( function(v) {
-	if ( v && v != '' )
-	    password1_msg( 'good' );
-	else
-	    password1_msg( 'required' );
-    });
-
-    var password2_msg = ko.observable( 'required' );
-    password2.subscribe( function(v) {
-	if ( v && v != '' && v == password1() )
-	    password2_msg( 'good' );
-	else if ( ! v || v == '' )
-	    password2_msg( 'required' );
-	else
-	    password2_msg( 'does not match' );
-    });
+    var view;
 
     return {
-	displayName: 'Registration',
-	email: email,
-	password1: password1,
-	password2: password2,
+	password: password,
 	displayname: displayname,
-	edittable_email: edittable_email,
-	accept: accept,
-	terms: terms,
 	validated: validated,
-	displayname_msg: displayname_msg,
-	email_msg: email_msg,
-	email_valid: email_valid,
-	password1_msg: password1_msg,
-	password2_msg: password2_msg,
+	labelShowHide: labelShowHide,
+
+	toggleShowHide: function() {
+	    var t = $(view).find('input[name="password"]').attr( 'type' );
+	    if ( t == 'password' ) {
+		labelShowHide( 'obscure' );
+		var t = $(view).find('input[name="password"]').attr( 'type', 'text' );
+	    }
+	    else {
+		labelShowHide( 'reveal' );
+		var t = $(view).find('input[name="password"]').attr( 'type', 'password' );
+	    }
+	},
+
+	displayTerms: function() {
+	},
+
+	attached: function( el ) {
+	    view = el;
+	},
 
 	canActivate: function( args ) {
 	    // In the case when/if user has already registered and perhaps
@@ -101,22 +62,30 @@ define(['plugins/router','lib/viblio','durandal/system'], function( router, vibl
 	activate: function( args ) {
 	    if ( args ) {
 		if ( args.email ) {
-		    email( args.email );
-		    edittable_email( false );
+		    email = args.email;
 		}
 		if ( args.url ) {
 		    url = args.url;
 		}
 	    }
-	    viblio.api( '/services/na/terms' ).then( function( json ) {
-	    	terms( json.terms );
-	    });
+	    return viblio.api( 
+		'/services/na/find_share_info_for_pending',
+		{ email: email } ).then( function( json ) {
+		    if ( json.owner ) {
+			displayname( json.owner.displayname );
+		    }
+		    else {
+			displayname( 'Someone' );
+		    }
+		    // We also have the mediafile (json.media ) and so
+		    // could display the poster, et. al. here.
+		});
 	},
 
 	done: function() {
-	    viblio.api( '/services/na/new_user', { email: email(),
-						   password: password1(),
-						   displayname: displayname() } )
+	    viblio.api( '/services/na/new_user', { email: email,
+						   password: password(),
+						   displayname: email } )
 		.then( function() {
 		    router.navigate( viblio.getLastAttempt() || url || '#/home' );
 		});
