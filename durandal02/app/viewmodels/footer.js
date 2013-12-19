8f11a8b583b8eef802422740831aba53bda15046
Jesse Garrison <jesse@gnarwal.com>
2013-12-18 17:06:42 -0800

    updates to api call, added alertify message on success

--- durandal02/app/viewmodels/footer.js
+++ durandal02/app/viewmodels/footer.js
@@ -1,20 +1,26 @@
 define( ['plugins/router', 'durandal/app', 'lib/viblio', 'lib/config'], function(router, app, viblio, config) {
     var feedback = ko.observable();
-    var feedback_email = ko.observable( 'mailto:feedback@' + config.email_domain() );
-    var getLocation = function() {
+    var feedback_email = ko.observable( 'feedback@' + config.email_domain() );
+    var feedback_location = function() {
         return window.location.hash;
     };
     
     return {
         feedback: feedback,
         feedback_email: feedback_email,
-        getLocation: getLocation,
+        feedback_location: feedback_location,
         
         send_feedback: function() {
-	    // add api call to submit feedback form
-            viblio.log( feedback() );
-            viblio.log( getLocation() );
-            viblio.log( feedback_email() );
+            var args;
+            args = {
+                feedback: feedback(),
+                feedback_email: feedback_email(),
+                feedback_location: feedback_location()
+            };
+            viblio.api('/services/na/form_feedback', args).then(function(){
+                viblio.mpEvent( 'feedback' );
+		viblio.notify( 'Thank you for your feedback', 'success' );
+            });
 	}
     };
 });
