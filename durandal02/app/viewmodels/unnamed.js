8456020a8cb64c6964493ca46cbfaff6760f3cb8
Andrew Peebles <peebles@video-analytics-inc.com>
2014-01-21 10:01:51 -0800

    fx

--- durandal02/app/viewmodels/unnamed.js
+++ durandal02/app/viewmodels/unnamed.js
@@ -15,25 +15,36 @@ function (router, system, app, viblio, Face, dialogs) {
     var view;
     var visible = ko.observable( false );
     var faces = ko.observableArray([]);
+    var allFaces = [];
+    var start = 0;
+    var numFacesTagged = 0;
     var pager = {
             next_page: 1,
-            entries_per_page: 3,
+            entries_per_page: 30,
             total_entries: -1 /* currently unknown */
         };
     var searching = ko.observable( false );
+
+    app.on( 'person:tag3_changed', function() {
+	numFacesTagged += 1;
+	if ( numFacesTagged >= allFaces.length ) {
+	    visible( false );
+	    app.trigger( 'unnamed:visibility', false );
+	}
+    });
 
     function addFace( face ) {
 	var f = new Face( face, {
 	    show_name: false,
 	    show_tag3: true,
 	} );
-	faces.push( f );
+	allFaces.push( f );
     }
 
     function search() {
 	if ( pager.next_page ) {
 	    searching( true );
-	    $(view).find('.fx').slideUp(function() {
+	    $(view).find('.fx').animate({'margin-left':'-312px'}, function() {
 		faces.removeAll();
 		viblio.api( '/services/faces/unnamed', 
 			    { page: pager.next_page, rows: pager.entries_per_page } )
@@ -42,7 +53,13 @@ function (router, system, app, viblio, Face, dialogs) {
 			data.faces.forEach( function( face ) {
 			    addFace( face );
 			});
-			$(view).find('.fx').slideDown();
+
+			var end = start + 3;
+			if ( end > ( allFaces.length + 1 ) )
+			    end = allFaces.length + 1;
+			faces( allFaces.slice( start, end ) );
+
+			$(view).find('.fx').animate({'margin-left':'0px'});
 			if ( data.faces.length ) {
 			    visible( true );
 			    app.trigger( 'unnamed:visibility', true );
@@ -63,13 +80,29 @@ function (router, system, app, viblio, Face, dialogs) {
 	searching: searching,
 
 	nextThree: function() {
-	    search();
+	    if ( start + 3 >= allFaces.length ) return;
+	    start += 3;
+	    var end = start + 3;
+	    if ( end > ( allFaces.length + 1 ) )
+		end = allFaces.length + 1;
+	    $(view).find('.fx').animate({'margin-left':'-312px'}, function() {
+		faces( allFaces.slice( start, end ) );
+		$(view).find('.fx').animate({'margin-left':'0px'});
+	    });
 	},
 
 	prevThree: function() {
-	    //search();
-	    visible( false );
-	    app.trigger( 'unnamed:visibility', false );
+	    if ( start == 0 ) return;
+	    start -= 3;
+	    if ( start < 0 )
+		start = 0;
+	    var end = start + 3;
+	    if ( end > ( allFaces.length + 1 ) )
+		end = allFaces.length + 1;
+	    $(view).find('.fx').animate({'margin-left':'312px'}, function() {
+		faces( allFaces.slice( start, end ) );
+		$(view).find('.fx').animate({'margin-left':'0px'});
+	    });
 	},
 
 	manageAll: function() {
