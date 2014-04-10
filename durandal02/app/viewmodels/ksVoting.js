db8f135608d67e288308f534fc9f25511b840391
Jesse Garrison <jesse@gnarwal.com>
2014-04-10 14:56:28 -0700

    updated vote page to match mocks

--- durandal02/app/viewmodels/ksVoting.js
+++ durandal02/app/viewmodels/ksVoting.js
@@ -1,4 +1,6 @@
-define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio'], function( router, whoWeAre, viblio ) {
+define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio', 'plugins/dialog'], function( router, whoWeAre, viblio, dialog ) {
+    
+    var clicks = ko.observable( 0 );
     
     var voteEmail = ko.observable('');
     var voteCode = ko.observable('');
@@ -59,7 +61,7 @@ define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio'], function( router
         }
     });
     var activityFormReady = ko.computed(function() {
-        if ( voteEmailValid() && voteCodeValid() && chosenVote() && chosenVote() != 'Choose...' ) {
+        if ( voteEmailValid() && voteCode() && chosenVote() && chosenVote() != 'Choose...' ) {
             if ( chosenVote() != 'other' ) {
                 return true;
             } else if ( ownActivity() && isOwnActivityValid() ) {
@@ -73,7 +75,15 @@ define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio'], function( router
     });
     
     function send_vote() {
-        var subject = 'Next activity vote submission'
+        var subject;
+        if ( voteCodeValid() ) {
+            subject = 'Next activity vote submission - valid code';
+        } else {
+            subject = 'Next activity vote submission - invalid code';
+        }
+        
+        // Send the vote regardless, but with correct subject. 
+        // Only difference to user is which modal they see after vote submission
         $.ajax({
             url: '/services/na/emailer',
             method: 'POST',
@@ -84,15 +94,24 @@ define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio'], function( router
                 body: '<p>Email: ' + voteEmail() + '</p>\n\
                        <p>Chosen vote: ' + voteToSubmit() + '</p>'
             })
-        }).always( function() {
-            // dont stress over the result
-            viblio.notify( 'Email Sent', 'success' );
         }).then( function() {
-            voteEmail('');
-            voteCode('');
-            chosenVote('Choose...');
+            if ( voteCodeValid() ) {
+                viblio.notify( 'Email Sent', 'success' );
+                voteEmail('');
+                voteCode('');
+                chosenVote('Choose...');
+                dialog.show('viewmodels/ksVoteThankYouModal');
+            } else {
+                dialog.show('viewmodels/ksVoteOopsModal');
+            }
         });
     }
+    
+    var showThankYouModal = function() {
+        if ( clicks() == 3 ) {
+            dialog.show('viewmodels/ksThankYouModal');
+        }
+    };
     
     return {
         voteEmail: voteEmail,
@@ -104,6 +123,58 @@ define(['plugins/router', 'viewmodels/whoWeAre', 'lib/viblio'], function( router
         ownActivityCharsLeft: ownActivityCharsLeft,
         activityFormReady: activityFormReady,
         
-        send_vote: send_vote        
+        send_vote: send_vote,
+        
+        compositionComplete: function() {
+            
+            $('button').on('click', function() {
+                clicks( clicks()+1 );
+                var percent = Math.min(Math.round(clicks() / 3 * 100), 100);
+                $('.percent').width(percent + '%');
+                $('.number').text(percent + '%');
+            });
+            
+            var ksurl = 'https://viblio.com';
+            
+            $('.facebook').on('click', function() {
+                var w = 580, h = 300,
+                        left = (screen.width/2)-(w/2),
+                        top = (screen.height/2)-(h/2);
+
+
+                    if ((screen.width < 480) || (screen.height < 480)) {
+                        window.open ('http://www.facebook.com/share.php?u=' + ksurl, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+                    } else {
+                        window.open ('http://www.facebook.com/share.php?u=' + ksurl, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);   
+                    }
+                    
+                    showThankYouModal();
+            });
+
+            $('.twitter').on('click', function() {
+                var loc = encodeURIComponent(ksurl),
+                        title = "I’m supporting VIBLIO on Kickstarter.  Check it out.",
+                        w = 580, h = 300,
+                        left = (screen.width/2)-(w/2),
+                        top = (screen.height/2)-(h/2);
+
+                    window.open('http://twitter.com/share?text=' + title + '&url=' + loc, '', 'height=' + h + ', width=' + w + ', top='+top +', left='+ left +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+                    
+                    showThankYouModal();
+            });
+
+            $('.linkedin').on('click', function() {
+                var loc = encodeURIComponent(ksurl),
+                        title = encodeURIComponent("I’m supporting VIBLIO on Kickstarter.  Check it out."),
+                        source = encodeURIComponent('Kickstarter'),
+                        w = 580, h = 550,
+                        left = (screen.width/2)-(w/2),
+                        top = (screen.height/2)-(h/2);
+
+                    window.open('http://www.linkedin.com/shareArticle?mini=true&url=' + loc + '&title=' + title + '&source=' + source, 'social', 'height=' + h + ', width=' + w + ', top='+top +', left='+ left +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+                    
+                    showThankYouModal();
+            });
+        }
     };
 });
