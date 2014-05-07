defc1cf8161a812cb87120eda14937791777259a
Jesse Garrison <jesse@gnarwal.com>
2014-05-07 14:03:43 -0700

    Added ability to add faces to new or existing album

--- durandal02/app/viewmodels/fscroll.js
+++ durandal02/app/viewmodels/fscroll.js
@@ -32,6 +32,10 @@ define(['plugins/router', 'durandal/app', 'durandal/system', 'lib/viblio', 'view
             entries_per_page: 15,
             total_entries: -1 /* currently unknown */
         };
+        
+        // used to add to or create new album from videos
+        self.albumLabels = ko.observableArray();
+        self.selectedAlbum = ko.observable(); 
 
 	self.contact_id = null;
         
@@ -127,9 +131,101 @@ define(['plugins/router', 'durandal/app', 'durandal/system', 'lib/viblio', 'view
 		});
 	    });
     };
+    
+    FScroll.prototype.getAllAlbumsLabels = function() {
+        var self = this;
+        var args = {};
+        args = {
+            cid: self.cid
+        };
+        self.albumLabels.removeAll();
+        viblio.api( '/services/album/list', args ).then( function(data) {
+            data.albums.forEach( function( album ) {
+                var _album = album;
+                _album.label = album.title;
+                _album.selected = ko.observable( false );
+                
+                self.albumLabels.push( _album );
+            });
+            self.albumLabels.unshift( {label: "Create New Album", selected: ko.observable(false)} );
+        });
+    };
+    
+    FScroll.prototype.addToAlbum = function( parent, album, callback ) {
+        var self = this;
+        
+        var albumMedia;
+        
+        if ( parent.mediafiles().length > 0 ) {
+            // Get a fresh list of media in album every time new media is added
+            viblio.api( 'services/album/get?aid=' + album.uuid ).then( function( data ) {
+                albumMedia = data.album.media;
+                
+                parent.mediafiles().forEach( function( mf ) {
+                    var present = false;
+                    albumMedia.forEach( function( albumMf ) {
+                       if( mf.view.id === albumMf.uuid ) {
+                           present = true;
+                       } 
+                    });
+
+                   if ( present ) {
+                        // No dups!
+                        return;
+                   } else {
+                       viblio.api( '/services/album/add_media', { aid: album.uuid, mid: mf.view.id } ).then( function() {
+                            app.trigger('album:newMediaAdded', album);
+                        });
+                   }
+                });
+                if( callback ) {
+                    callback();
+                }
+            });    
+        }
+    };
+    
+    FScroll.prototype.createNewAlbum = function() {
+        var self = this;
+        
+        if ( self.mediafiles().length > 0 ) {
+            viblio.api( '/services/album/create', { name: 'Click to name this album', initial_mid: self.mediafiles()[0].media().uuid } ).then( function( data ) {
+                self.addToAlbum( self, data.album, function() {
+                    router.navigate( 'viewAlbum?aid=' + data.album.uuid );
+                });
+            });
+        }
+    };
+    
+    FScroll.prototype.albumSelected = function( self, album ) {
+        self.albumLabels().forEach( function( a ) {
+            a.selected( false );
+        });
+        album.selected( true );
+        self.selectedAlbum( album );     
+    };
+    
+    FScroll.prototype.addOrCreateAlbum = function() {
+        var self = this;
+        
+        if( self.selectedAlbum().label === 'Create New Album' ) {
+            self.createNewAlbum();
+        } else {
+            self.addToAlbum( self, self.selectedAlbum(), function() {
+                var vidOrVids = self.mediafiles().length == 1 ? ' video' : ' videos';
+                var msg = self.mediafiles().length + vidOrVids + ' successfully added to "' + self.selectedAlbum().label + '"';
+                viblio.notify( msg, 'success' );
+            });
+            // Used to close the dropdown
+            $("body").trigger("click");
+        }
+    };
 
     FScroll.prototype.activate = function() {
 	var self = this;
+        
+        // get list of albums to create dropdown menu
+        self.getAllAlbumsLabels();
     };
 
     FScroll.prototype.seeAll = function() {
