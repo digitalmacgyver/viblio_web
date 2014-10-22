3e157a32cf9fc01c3009eb7dd95100d037802b49
Jesse Garrison <jesse@gnarwal.com>
2014-10-22 12:17:32 -0700

    removed special callout for ipadUrl in faq

--- durandal02/app/viewmodels/faq.js
+++ durandal02/app/viewmodels/faq.js
@@ -113,9 +113,7 @@ define( ['plugins/router',
             
             $(".faqVid-Wrap").flowplayer({ src: "lib/flowplayer/flowplayer-3.2.16.swf", wmode: 'opaque' }, {
                 clip: {
-                    url: 'https://viblio.com/s/e/5221DA02-565A-11E4-AFCA-4B6695BB129F',
-                    //ipadUrl: encodeURIComponent('https://viblio.com/s/e/5221DA02-565A-11E4-AFCA-4B6695BB129F')
-                    ipadUrl: 'https://viblio.com/s/e/5221DA02-565A-11E4-AFCA-4B6695BB129F.mp4'
+                    url: 'https://viblio.com/s/e/5221DA02-565A-11E4-AFCA-4B6695BB129F'
                 },
                 onStart: function( clip ) {
                     flowplayer().mute();    
