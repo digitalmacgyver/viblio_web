71b08118f9a9bcf10dad61c261bd8e44c6fa39cb
Jesse Garrison <jesse@gnarwal.com>
2014-10-24 16:32:09 -0700

    change access to upload results

--- durandal02/app/viewmodels/coverphoto.js
+++ durandal02/app/viewmodels/coverphoto.js
@@ -133,7 +133,7 @@ function(router, app, viblio, config, dialogs, Events, dialog) {
                 },
                 done: function (e, data) {
                     console.log( data, data.result );
-                    backgroundImageUrl( data.result.views.banner.url );
+                    backgroundImageUrl( data.result[0].views.banner.url );
                 }
             });
             
