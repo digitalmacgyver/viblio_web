7d2597ef6af5d8af28dcdea48aaf2f08c798fa15
Jesse Garrison <jesse@gnarwal.com>
2014-10-24 09:04:07 -0700

    code test for upload

--- durandal02/app/viewmodels/coverphoto.js
+++ durandal02/app/viewmodels/coverphoto.js
@@ -126,6 +126,32 @@ function(router, app, viblio, config, dialogs, Events, dialog) {
                 });
             });*/
             
+            
+            
+            
+                
+                // Change this to the location of your server-side upload handler:
+                var url = '/services/user/add_or_replace_banner_photo/';
+                $('#fileupload').fileupload({
+                    url: url,
+                    dataType: 'json',
+                    done: function (e, data) {
+                        $.each(data.result.files, function (index, file) {
+                            $('<p/>').text(file.name).appendTo('#files');
+                        });
+                    },
+                    progressall: function (e, data) {
+                        var progress = parseInt(data.loaded / data.total * 100, 10);
+                        $('#progress .progress-bar').css(
+                            'width',
+                            progress + '%'
+                        );
+                    }
+                }).prop('disabled', !$.support.fileInput)
+                    .parent().addClass($.support.fileInput ? undefined : 'disabled');
+            
+            
+            
             app.trigger( 'coverphoto:composed', this );
 	}
     
