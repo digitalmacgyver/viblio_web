756243a4e202dd69f5367994e7de48f148bb3548
Jesse Garrison <jesse@gnarwal.com>
2014-12-18 14:59:20 -0800

    Updated flow player background color, added missing comma in cover photos, fixed splash page sinner

--- durandal02/app/viewmodels/pp.js
+++ durandal02/app/viewmodels/pp.js
@@ -437,8 +437,9 @@ define(["durandal/app",
                 }
             },
             canvas: {
-                backgroundColor:'#254558',
-                backgroundGradient: [0.1, 0]
+                backgroundColor:'rgba(239, 239, 232,1)'
+                //backgroundColor:'#254558',
+                //backgroundGradient: [0.1, 0]
             }
         }).flowplayer().ipad({simulateiDevice: should_simulate()});
 	resizePlayer();
